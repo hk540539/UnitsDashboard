17ddb33a39c8475c95619451c4e63c88b5a52062
hemanth kumar <hemanth.sakamuri@imaginate.in>
2020-07-19 19:58:42 +0530

    make it better

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,9 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import store from "./store/store";
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
   document.getElementById("root")
 );
 
